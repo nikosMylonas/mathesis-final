6717e2a67094f169a96e4787848cfca13cf5a6d9
Nikos <sokinsanolym@gmail.com>
2024-11-06 12:43:50 +0200

    Render deployment

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,23 +1,27 @@
 import 'dotenv/config';
 import { Sequelize } from "sequelize";
 
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
+/* For local environment */
+// const dbName = process.env.DB_NAME;
+// const dbUser = process.env.DB_USERNAME;
+// const dbPassword = process.env.DB_PASSWORD;
+// const dbHost = process.env.DB_HOST;
+// const dbPort = process.env.DB_PORT;
 
-const sequelize = new Sequelize({
-    host: dbHost,
-    dialect: 'postgres',
-    port: dbPort,
-    username: dbUser,
-    database: dbName,
-    password: dbPassword,
-    logging: false,
-    define: {
-        timestamps: false
-    }
-});
+const sequelize = new Sequelize(
+    /* Render environment */
+    process.env.DATABASE_URL,
+    {
+        // host: dbHost,
+        dialect: 'postgres',
+        // port: dbPort,
+        // username: dbUser,
+        // database: dbName,
+        // password: dbPassword,
+        logging: false,
+        define: {
+            timestamps: false
+        }
+    });
 
 export { sequelize };
